42178990c3a7b84ecf1a8bba357a2c5bc062914f
SimoneChiodo <simochi.005@gmail.com>
2025-02-27 16:39:25 +0100

    Add new constructor; Fix error

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -2,8 +2,19 @@ package org.lessons.java.shop;
 
 public class Main {
     public static void main(String[] args) {
-      Prodotto product = new Prodotto();
+      // // Create a new istance
+      // Prodotto product = new Prodotto();
 
+      // // Print the results
+      // System.err.println("Il prezzo del prodotto e': " + product.getPrice());
+      // System.err.println("Il prezzo del prodotto + IVA e': " + product.getPriceWithIva());
+      // System.err.println("Il nome del prodotto e': " + product.getName());
+
+      
+      // Create a new istance
+      Prodotto product = new Prodotto("Shampoo", "Just a normal shampoo", 4.99f, 22);
+
+      // Print the results
       System.err.println("Il prezzo del prodotto e': " + product.getPrice());
       System.err.println("Il prezzo del prodotto + IVA e': " + product.getPriceWithIva());
       System.err.println("Il nome del prodotto e': " + product.getName());
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -5,18 +5,35 @@ import java.util.Random;
 
 public class Prodotto {
 
+  // Attributes ---------------
   int code;
   String name;
   String description;
   float price;
   float iva;
 
+
+  // Methods ------------------
   Prodotto(){
     // Create a new random
     Random random = new Random();
 
     // Assign randomly a productCode
     code = random.nextInt(999);
+  }
+
+  Prodotto(String name, String description, float price, float iva){
+    // Create a new random
+    Random random = new Random();
+
+    // Assign randomly a productCode
+    code = random.nextInt(999);
+
+    // Initialize other attributes
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.iva = iva;
   }
 
   float getPrice(){
@@ -28,7 +45,7 @@ public class Prodotto {
   }
 
   String getName(){
-    return code + name;
+    return code + "-" + name;
   }
 
 }
