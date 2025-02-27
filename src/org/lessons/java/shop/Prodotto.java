2aadf0bc0a1b2e019715b2031cded43ec92c02ba
SimoneChiodo <simochi.005@gmail.com>
2025-02-27 16:28:01 +0100

    Add methods

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -11,4 +11,24 @@ public class Prodotto {
   float price;
   float iva;
 
+  Prodotto(){
+    // Create a new random
+    Random random = new Random();
+
+    // Assign randomly a productCode
+    code = random.nextInt(999);
+  }
+
+  float getPrice(){
+    return price;
+  }
+
+  float getPriceWithIva(){
+    return price * (1 + iva / 100);
+  }
+
+  String getName(){
+    return code + name;
+  }
+
 }
